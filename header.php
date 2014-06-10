6650e6713ccf8f5d734ab55f2c7e09040959de45
Schweppers <blazerowland5@gmail.com>
2014-06-10 11:05:56 -0500

    Files were good ;)

---
+++ header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="author" content="Blaze Rowland"/>
+		<meta name="keywords" content="Eminem, Slim Shady, Rap, HTML"/>
+		<title>EMINEM</title>
+		<link rel="shortcut icon" href="includes/images/favicon.ico">
+		<link href='http://fonts.googleapis.com/css?family=Wallpoet' rel='stylesheet' type='text/css'>
+		<style>@import 'includes/css/screen.css';</style>
+	</head>
+	<body>
+		<div id="header">
+			<span class="header_span"><a href="index.php"><p class="header">EMINEM</p></a></span>
+		</span></div>
+	</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<span class="menu_span"><div id="menu" style="word-spacing:26px;">
+		<a href="index.php" class="menu"><p class="menu">Home |</p></a>
+		<a href="email.php" class="menu"><p class="menu">Email |</p></a>
+		<a href="video.php" class="menu"><p class="menu">Video</p></a>
+	</div></span>
